dd8eae1e3cdc356b5b9b9bee99657bd3a7a379e9
Amauris Peña Díaz <amauris@outlook.com>
2022-11-22 19:37:20 -0400

    In the process of creating ImmediateErrorListener

---
+++ ImmediateErrorListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace Calculadora
+{
+    internal class ImmediateErrorListener : BaseErrorListener
+    {
+        public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            //throw new ParseCancellationException("line " + line + ":" + charPositionInLine + " " + msg);
+        }
+    }
+}
